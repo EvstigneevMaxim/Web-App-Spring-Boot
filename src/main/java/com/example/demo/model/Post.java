acf8c5cdf9b1c84cc621add2b5502b29d9f3e0b5
Maxim <maksimevs2003@gmail.com>
2024-10-02 00:22:50 +0500

    Bootstrap верстка

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,8 +1,12 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+
+    private Date creationDate;
 
     public String getText() {
         return text;
@@ -12,6 +16,11 @@ public class Post {
         return likes;
     }
 
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+
     public Post(String text, Integer likes) {
         this.text = text;
         this.likes = likes;
@@ -20,5 +29,10 @@ public class Post {
     public Post(String text) {
         this.text = text;
         this.likes = 0;
+    }
+
+    public Post(String text, Date creationDate) {
+        this.text = text;
+        this.creationDate = creationDate;
     }
 }
