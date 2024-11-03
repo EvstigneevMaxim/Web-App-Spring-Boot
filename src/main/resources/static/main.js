6685f04814b1c66b9fc0f17ca50d3354d14b9fe5
Maxim <maksimevs2003@gmail.com>
2024-11-03 20:31:47 +0500

    Add active likeButton

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,5 +1,20 @@
-window.onload = function() {
+/*window.onload = function() {
+    //alert('Страница загружена');
     $(".like").click((e) => {
        alert("debug: button text: " + e.currentTarget.innerText)
     });
-};
+};*/
+
+window.onload = function () {
+    $(".like").click((e) => {
+        var btn = $(e.currentTarget);
+        var postId = btn.attr("data-post-id");
+        btn.attr("disabled", "disabled");
+        $.post("/post/" + postId + "/like", (data) => {
+           btn.text("" + data + " ❤");
+           btn.removeClass("btn-secondary");
+           btn.addClass("btn-danger");
+        });
+    });
+}
+
